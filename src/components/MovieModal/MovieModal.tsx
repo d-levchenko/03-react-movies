638d916b526a8f8796a9999ffd763ddf7218094e
d-levchenko <dl.qubb@gmail.com>
2026-04-09 18:34:49 +0300

    completed modal window

--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -1,6 +1,7 @@
 import { createPortal } from 'react-dom';
 import type { Movie } from '../../types/movie';
 import css from './MovieModal.module.css';
+import { useEffect } from 'react';
 
 interface MovieModalProps {
   movie: Movie | null;
@@ -8,12 +9,42 @@ interface MovieModalProps {
 }
 
 const MovieModal = ({ movie, onClose }: MovieModalProps) => {
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    if (!movie) {
+      return;
+    }
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.body.style.overflow = 'visible';
+    };
+  }, [movie, onClose]);
+
+  if (!movie) {
+    return null;
+  }
+
   return createPortal(
     <div
       className={css.backdrop}
       role="dialog"
       aria-modal="true"
-      onClick={onClose}>
+      onClick={handleBackdropClick}>
       <div className={css.modal}>
         <button
           className={css.closeButton}
@@ -22,18 +53,18 @@ const MovieModal = ({ movie, onClose }: MovieModalProps) => {
           &times;
         </button>
         <img
-          src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
-          alt={movie?.title}
+          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+          alt={movie.title}
           className={css.image}
         />
         <div className={css.content}>
-          <h2>{movie?.title}</h2>
-          <p>{movie?.overview}</p>
+          <h2>{movie.title}</h2>
+          <p>{movie.overview}</p>
           <p>
-            <strong>Release Date:</strong> {movie?.release_date}
+            <strong>Release Date:</strong> {movie.release_date}
           </p>
           <p>
-            <strong>Rating:</strong> {movie?.vote_average}
+            <strong>Rating:</strong> {movie.vote_average}
           </p>
         </div>
       </div>
